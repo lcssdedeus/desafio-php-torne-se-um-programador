bfba055fe6e4bee4f40481be44baab6486be893a
LucasdeDeus <lcssdedeus@gmail.com>
2025-04-28 12:20:09 -0300

    exercício 1

--- init.php
+++ init.php
@@ -1,6 +1,12 @@
 <?php
    
-   echo "Digite algo: ";
+   $teste = 1;
+   $teste2 = 2;
+   $resultado = $teste + $teste2;
+
+   echo $resultado;
+
+/*    echo "Digite algo: ";
    $entrada = fgets(STDIN);
 
-   echo "Você digitou: " . $entrada;
+   echo "Você digitou: " . $entrada; */
